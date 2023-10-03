2f4f751c3881e54264877b41de6d00504fd39d94
cnncnnzh@gmail.com <cnncnnzh@gmail.com>
2023-10-03 01:37:44 -0500

    Edit model.py and train.py

--- cgcnn_ours/model.py
+++ cgcnn_ours/model.py
@@ -10,11 +10,15 @@ from torch_geometric.utils import add_self_loops, degree
 
 
 class GCNN(torch.nn.Module):
+    '''
+    main graph concolutional neuron network class
+    '''
     def __init__(self,
                  in_dim,
                  out_dim,
                  hidden_dim=64,
-                 n_conv_layer=3):
+                 n_conv_layer=3,
+                 n_linear=1):
         super(GCNN, self).__init__()
 
         # setup linear layer before gnn
@@ -31,43 +35,37 @@ class GCNN(torch.nn.Module):
             
         # Batch normalization for fast convergence
         self.batch_norm = BatchNorm1d(hidden_dim)
+        
+        # Post cgnn layers
+        self.mlp = MLP(hidden_dim, n_linear)
     
     def forward(self, data):
         x, edge_index, edge_attr, symmetry, global_idx, target = \
             data.x, data.edge_index, data.edge_attr, data.symmetry, data.global_idx, data.y
+        
+        # pre cgnn
+        x = self.act(self.lin(x))
+        
+        # cgnn
+        for conv in self.conv_list:
+            x = self.batch_norm(conv(x, edge_index, edge_attr))
+        
+        # add global featrures here
+        
+        
+        # post cgnn
+        x = self.mlp(x)
 
-        # Step 1: Add self-loops to the adjacency matrix.
-        edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
+        return x
 
-        # Step 2: Linearly transform node feature matrix.
-        x = self.lin(x)
-
-        # Step 3: Compute normalization.
-        row, col = edge_index
-        deg = degree(col, x.size(0), dtype=x.dtype)
-        deg_inv_sqrt = deg.pow(-0.5)
-        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
-        norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
-
-        # Step 4-5: Start propagating messages.
-        out = self.propagate(edge_index, x=x, norm=norm)
-
-        # Step 6: Apply a final bias vector.
-        out += self.bias
-
-        return out
-
-    def message(self, x_j, norm):
-        # x_j has shape [E, out_channels]
-
-        # Step 4: Normalize node features.
-        return norm.view(-1, 1) * x_j
     
 class MLP(torch.nn.Module):
-    # multilayer perceptron after message passing layers
+    '''
+    multilayer perceptron after message passing layers
+    '''
     def __init__(self,
                  hidden_dim,
-                 n_linear=1):
+                 n_linear):
         super(MLP, self).__init__()
         self.lin_list = ModuleList()
         for _ in range(n_linear):
@@ -76,4 +74,13 @@ class MLP(torch.nn.Module):
         self.out_1 = Linear(hidden_dim, 32)
         self.out_2 = Linear(32, 1)
         
-    def forward(self, data):
+    def forward(self, x):
+        for lin in self.lin_list:
+            x = lin(x)
+            x = self.act(x)
+        x = self.out_1(x)
+        x = self.act(x)
+        x = self.out_2(x)
+        x = self.act(x)
+        return x
+            
--- cgcnn_ours/train.py
+++ cgcnn_ours/train.py
@@ -3,43 +3,101 @@
 Created on Wed May 31 23:34:03 2023
 """
 
-from data import generate_dataset, data_loader
+import os
+
+from cgcnn_ours.data import generate_dataset, data_loader
+from cgcnn_ours.model import GCNN
 import torch
+from torch import nn
+from torch import optim
 
-def train(data_options,
-         batch_options,
-         train_options,
-         device,):
-    dataset = generate_dataset(root_dir, data_options)
-    train_loader, val_loader, test_loader = data_loader(dataset, batch_options)
-    
-    
-    
-def train_model(train_loader, model, criterion, optimizer, epoch):
-    """
-    main function of training model —— load data, build model, main training loop,
-    result 
-    """
-    
-    pass
-
-
-
-if __name__ == '__main__':
+def train(
+    root_dir,
+    data_options,
+    batch_options,
+    train_options,
+    model_options
+):
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    dataset = generate_dataset(root_dir, data_options)
+    
+    in_dim = dataset.num_features
+    train_loader, val_loader, test_loader = data_loader(dataset, batch_options)
+
+
+    model = GCNN(in_dim, 1, **model_options)
+    # load existed model
+    saved_model = os.path.join(root_dir, 'saved_model.pt')
+    if os.path.exists(saved_model):
+        model.load_state_dict(torch.load(saved_model, map_location=device))
+    model.to(device=device)
+    
+    # setup the optimizer
+    optimizer = getattr(optim, train_options["optimizer"])(
+        model.parameters(),
+        lr=train_options['lr'],
+    )   
+    
+    # setup scheduler
+    scheduler = getattr(optim, train_options["scheduler"])(optimizer)
+    
+    #setup loss fnction
+    loss_func = getattr(nn, train_options['loss_func'])
+    
+    # the main training loop
+    train_losses, train_losses, train_losses = [], [], []
+    best_score = float('inf')
+    for epoch in range(train_options['batch']):
+        model.train()
+        epoch_train_loss = 0
+        count = 0
+        
+        # train and calculate training error
+        for data in train_loader:
+            pred = model(data)
+            optimizer.zero_grad()
+            loss = loss_func(pred, data.y)
+            loss.backward()
+            optimizer.step()
+            
+            epoch_train_loss += loss.item()
+            count += pred.size(0)
+        train_losses.append(epoch_train_loss/count) 
+    
+
+    # save model
+    torch.save(model.state_dict(), saved_model)
+    
+if __name__ == '__main__':
     
     root_dir = r'D:\Dropbox\Vasp_home\Machine_learning\machine-learning\cgcnn_ours\tests'
-    data_options = {'max_num_nbr':12,
-                    'radius':8,
-                    'dmin':0,
-                    'step':0.2,
-                    'random_seed':64}
-    batch_options = {'train_ratio':0.7,
-                     'val_ratio':0.15,
-                     'test_ratio':0.15,
-                     'batch_size':32,
-                     'num_workers':1}
-    train_options = {}
-
-    train_loader = main(data_options, batch_options, train_options)
+    data_options = {
+        'max_num_nbr':12,
+        'radius':8,
+        'dmin':0,
+        'step':0.2,
+        'random_seed':64
+    }
+    batch_options = {
+        'train_ratio':0.7,
+        'val_ratio':0.15,
+        'test_ratio':0.15,
+        'batch_size':32,
+        'num_workers':1
+    }
+    model_options = {
+        'hidden_dim':64,
+        'n_conv_layer':3,
+        'n_linear':1
+    }
+    train_options = {
+        'batch':20,
+        'optimizer':'Adam',
+        'lr':0.001,
+        'dropout':0.2,
+        'scheduler':'LambdaLR',
+        'loss_func':'MSELoss'
+    }
+    
+    train(data_options, batch_options, train_options)
